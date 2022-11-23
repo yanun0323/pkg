53e41aa8de8f1f3fd76b7aa37abcdc7863b799fb
yanun <yanunyang@psp-power.com.tw>
2022-11-23 14:13:23 +0800

    change config init implement

--- config/config.go
+++ config/config.go
@@ -1,45 +1,67 @@
 package config
 
 import (
+	"fmt"
 	"os"
-	"path/filepath"
-	"runtime"
+	"sort"
 	"strings"
-	"sync"
 
 	"github.com/spf13/viper"
+	"github.com/yanun0323/pkg/logs"
 )
 
-var (
-	once sync.Once
-)
+/*
+Initial the config from config.yaml
 
-func Init(cfgName string) error {
-	var err error
-	once.Do(
-		func() {
-			if cfgName == "" {
-				cfgName = "config"
-			}
-			_, f, _, _ := runtime.Caller(0)
-			cfgPath := filepath.Join(filepath.Dir(f), "../../config")
-			viper.AddConfigPath(cfgPath)
-			viper.AddConfigPath("./config")
-			configName := os.Getenv("CONFIG_NAME")
-			if configName != "" {
-				cfgName = configName
-			}
+	# Sample Code
 
-			viper.SetConfigName(cfgName)
-			viper.AutomaticEnv()
-			viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
-			viper.SetConfigType("yaml")
+		var _once sync.Once
 
-			err = viper.ReadInConfig()
-			if err != nil {
-				return
-			}
-		},
-	)
-	return err
+		func Init(cfgName string) error {
+			var err error
+			_once.Do(
+				func() {
+					_, f, _, _ := runtime.Caller(0)
+					cfgPath := filepath.Join(filepath.Dir(f), "../../config")
+					if err = config.Init(cfgPath, cfgName, true); err != nil {
+						return
+					}
+				},
+			)
+			return err
+		}
+*/
+func Init(cfgPath, cfgName string, dump bool) error {
+	viper.AddConfigPath(cfgPath)
+	viper.AddConfigPath("./config")
+	configName := os.Getenv("CONFIG_NAME")
+	if configName != "" {
+		cfgName = configName
+	}
+
+	viper.SetConfigName(cfgName)
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.SetConfigType("yaml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	if dump {
+		Dump()
+	}
+
+	return nil
+}
+
+func Dump() {
+	keys := viper.AllKeys()
+	sort.Strings(keys)
+	for _, key := range keys {
+		if strings.Contains(key, "password") || strings.Contains(key, "secret") || strings.Contains(key, "key") || strings.Contains(key, "pass") || strings.Contains(key, "pem") {
+			continue
+		}
+		logs.New("config", 0).Info(fmt.Sprintf("%s: %+v\n", key, viper.Get(key)))
+	}
 }
