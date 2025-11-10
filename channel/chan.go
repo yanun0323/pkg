846ee34665daa818ab87e281ae72dce54de5f864
Yanun Yang <yanun.yang@GTS-NB041.local>
2025-11-10 09:59:19 +0800

    fix: safe close race condition and try push returns bool

--- channel/chan.go
+++ channel/chan.go
@@ -1,9 +1,16 @@
 package channel
 
+import "sync"
+
+var safeCloseMu sync.Mutex
+
 // SafeClose closes a channel if it is open.
 //
 // It is safe to call this function even if the channel is already closed.
 func SafeClose[T any](ch chan T) {
+	safeCloseMu.Lock()
+	defer safeCloseMu.Unlock()
+
 	select {
 	case _, ok := <-ch:
 		if ok {
@@ -17,10 +24,12 @@ func SafeClose[T any](ch chan T) {
 // TryPush pushes a value to a channel. If the channel is full, it will skip the push without blocking.
 //
 // It is safe to call this function even if the channel is already closed.
-func TryPush[T any](ch chan<- T, data T) {
+func TryPush[T any](ch chan<- T, data T) bool {
 	select {
 	case ch <- data:
+		return true
 	default:
+		return false
 	}
 }
 
